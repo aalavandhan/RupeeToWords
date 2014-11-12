1caeafda9c60f909da6f1d4e3ec8a54870f2c4e1
Nithin Krishna <nithin@codebrahma.com>
2014-11-12 19:28:22 +0530

    expect syntax used for specs

--- spec/rupee_to_words_spec.rb
+++ spec/rupee_to_words_spec.rb
@@ -6,13 +6,13 @@ describe RupeeToWords do
 	end
 	context do
 		it "should convert 10 to Ten Rupees only" do
-			Test.to_words(10).should eq("Ten Rupees ,only.")
+			expect(Test.to_words(10)).to eq("Ten Rupees ,only.")
 		end
 		it "should convert 100.1 to Hundred Rupees and thirty five paisa only" do
-			Test.to_words(100.1).should eq("One Hundred Rupees and Ten Paisa ,only.")
+			expect(Test.to_words(100.1)).to eq("One Hundred Rupees and Ten Paisa ,only.")
 		end
 		it "should convert 1,12,33,02,344 to Hundred and twelve crore thiry three lakh two thousand three hundred and forty four only" do
-			Test.to_words(1123302344).should eq("One Hundred Twelve Crore Thirty Three Lakh Two Thousand Three Hundred Forty Four Rupees ,only.")
-		end	
+			expect(Test.to_words(1123302344)).to eq("One Hundred Twelve Crore Thirty Three Lakh Two Thousand Three Hundred Forty Four Rupees ,only.")
+		end
 	end
 end
